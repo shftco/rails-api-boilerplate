61f8e75c4f553a04fd9616c6d53331b5350a2e7f
nejdetkadir <nejdetkadir.550@gmail.com>
2022-02-13 01:57:07 +0300

    Create user and registration services

---
+++ app/services/registration_service/register.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "dry/monads/do"
+
+class RegistrationService::Register
+  include Dry::Monads[:result]
+  include Dry::Monads::Do.for(:call)
+
+  attr_reader :params
+
+  def initialize(params:)
+    @params = params
+  end
+
+  def call
+    ActiveRecord::Base.transaction(requires_new: true) do
+      user = yield call_create_user
+
+      Success(user)
+    end
+  end
+
+  private
+
+  def call_create_user
+    UserService::Create.new(params:).call
+  end
+end
---
+++ app/services/user_service/create.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "dry/monads/do"
+
+class UserService::Create
+  include Dry::Monads[:result]
+  include Dry::Monads::Do.for(:call)
+
+  attr_reader :params
+
+  def initialize(params:)
+    @params = params
+  end
+
+  def call
+    user = User.new(params)
+
+    if user.save
+      Success(user)
+    else
+      Failure([:error, user.errors])
+    end
+  end
+end
