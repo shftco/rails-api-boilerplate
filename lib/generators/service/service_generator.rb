3e285d5d79e38ccdc5cc20fb534c69353e738a7c
nejdetkadir <nejdetkadir.550@gmail.com>
2022-02-12 12:03:56 +0300

    Fix service name

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -9,20 +9,34 @@ class ServiceGenerator < Rails::Generators::NamedBase
     %w[create update destroy list].each do |action|
       template(
         "#{action}.html.erb",
-        File.join("app/services/#{create_service_name}_service", create_file_path, "#{action}.rb")
+        File.join("app/services/#{create_service_file_name}_service", create_file_path, "#{action}.rb")
       )
     end
   end
 
   private
 
+  def service_classes
+    class_name.split("::")
+  end
+
   def create_file_path
-    path = class_name.split("::")
+    path = service_classes
     path.shift
     path.join("/").underscore
   end
 
-  def create_service_name
+  def create_service_file_name
     (class_path.size.zero? ? file_path : class_path.first).singularize
+  end
+
+  def service_class_name
+    if service_classes.size == 1
+      "#{class_name.singularize.camelize}Service"
+    else
+      class_names = service_classes
+      class_names[0] = "#{class_names.first.singularize.camelize}Service"
+      class_names.join("::")
+    end
   end
 end
