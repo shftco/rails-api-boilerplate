d16f7a0df366af9b97cb92cf174c0e93c43f4dac
nejdetkadir <nejdetkadir.550@gmail.com>
2022-02-12 12:29:10 +0300

    Add tests

--- test/lib/generators/service_generator_test.rb
+++ test/lib/generators/service_generator_test.rb
@@ -8,9 +8,37 @@ class ServiceGeneratorTest < Rails::Generators::TestCase
   destination Rails.root.join("tmp/generators")
   setup :prepare_destination
 
-  # test "generator runs without errors" do
-  #   assert_nothing_raised do
-  #     run_generator ["arguments"]
-  #   end
-  # end
+  test 'generator runs without errors' do
+    assert_nothing_raised do
+      run_generator ["User"]
+    end
+  end
+
+  test 'generator creates service files' do
+    run_generator ["User"]
+
+    %w[create update destroy list].each do |action|
+      assert_file "app/services/user_service/#{action}.rb"
+    end
+  end
+
+  test 'generator creates service files with custom actions' do
+    run_generator ["User", "--actions", %w[create update]]
+
+    %w[create update].each do |action|
+      assert_file "app/services/user_service/#{action}.rb"
+    end
+
+    %w[destroy list].each do |action|
+      assert_no_file "app/services/user_service/#{action}.rb"
+    end
+  end
+
+  test 'generator creates service files with using module name' do
+    run_generator ["Facebook::Adset"]
+
+    %w[create update destroy list].each do |action|
+      assert_file "app/services/facebook_service/adset/#{action}.rb"
+    end
+  end
 end
