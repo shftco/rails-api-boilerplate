90e3b80a5d900231ef1237a29a590dc4d0499469
nejdetkadir <nejdetkadir.550@gmail.com>
2022-10-11 20:53:04 +0300

    Update contract generator

--- lib/generators/contract/contract_generator.rb
+++ lib/generators/contract/contract_generator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ContractGenerator < Rails::Generators::NamedBase
-  argument :params, type: :array, default: [], banner: 'action action'
+  argument :params, type: :array, default: [], banner: 'name:o fullname:required'
   class_option :parent, type: :string, default: 'ApplicationContract', desc: 'The parent class for the generated contract'
 
   check_class_collision suffix: 'Contract'
@@ -30,6 +30,39 @@ class ContractGenerator < Rails::Generators::NamedBase
 
   def parent_class_name
     options[:parent]
+  end
+
+  def formatted_params
+    params.map do |current_param|
+      type = type_defined_for_param?(current_param) ? defined_type_for_param(current_param) : 'optional'
+
+      [type, parse_param(current_param).first]
+    end
+  end
+
+  def parse_param(current_param)
+    current_param.split(':')
+  end
+
+  def type_defined_for_param?(current_param)
+    param = parse_param(current_param)
+
+    return false if param.size <= 1
+
+    valid_types = %w[r o required optional]
+
+    valid_types.include?(param.second)
+  end
+
+  def defined_type_for_param(current_param)
+    param = parse_param(current_param)
+
+    case param.last
+    when 'o', 'optional'
+      'optional'
+    when 'r', 'required'
+      'required'
+    end
   end
 
   def file_name
